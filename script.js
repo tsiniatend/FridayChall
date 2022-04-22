8569998f8c5380d772e7e8d506ad77ddc521f637
tsiniatend <100612815+tsiniatend@users.noreply.github.com>
2022-04-22 13:53:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const startProject = ["firstOne", "secondTwo", "thirdThree", "fourthFour", "fifthFive"];
+
+const partProject = [1, 2, 3, 4, 5];
+
+const aryProject = new Array(5).fill(false);
+
+// 👇️ [all naturally false)
+console.log(aryProject);
+
+
+
+//final int NUMBER_OF_STOVES = 5;
+//boolean[] isStoveTurnedOn = { true, false, false, true, true};
+
